7fcb85821a321eb9c22b790fbe3072c2ebbcfdf4
jeffersontdm <jeffersontavares1611@gmail.com>
2024-02-16 08:39:12 -0300

    added base to init project

---
+++ src/main/java/com/jeffersontdm/anotaaiteste/AnotaAiTesteApplication.java
@@ -0,0 +1,12 @@
+package com.jeffersontdm.anotaaiteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotaAiTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotaAiTesteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jeffersontdm/anotaaiteste/AnotaAiTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffersontdm.anotaaiteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotaAiTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
